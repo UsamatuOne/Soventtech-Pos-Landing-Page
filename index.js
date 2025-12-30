19e1e567307f8fdea7c3eacd993cb6158e1f7485
Usama <usamamuhammadharuna6@gmail.com>
2025-12-30 17:16:49 +0100

    done

--- index.js
+++ index.js
@@ -2,403 +2,403 @@
 
 
 gsap.registerPlugin(ScrollTrigger);
-window.addEventListener("load", () => {
+// window.addEventListener("load", () => {
 
-    // hero text animations
-    gsap.fromTo(".f-text", {
-        y: -80,
-        opacity: 0,
-    },
-        {
-            y: 0,
-            opacity: 1,
-            stagger: 0.1,
-            duration: 1,
-            delay: 0.5,
-            ease: "power3.out"
-        });
+//     // hero text animations
+//     gsap.fromTo(".f-text", {
+//         y: -80,
+//         opacity: 0,
+//     },
+//         {
+//             y: 0,
+//             opacity: 1,
+//             stagger: 0.1,
+//             duration: 1,
+//             delay: 0.5,
+//             ease: "power3.out"
+//         });
 
-    gsap.fromTo(".f-p", {
-        y: 80,
-        opacity: 0,
-    },
-        {
-            y: 0,
-            opacity: 1,
-            stagger: 0.1,
-            duration: 1,
-            delay: 0.3,
-            ease: "power3.out"
-        });
+//     gsap.fromTo(".f-p", {
+//         y: 80,
+//         opacity: 0,
+//     },
+//         {
+//             y: 0,
+//             opacity: 1,
+//             stagger: 0.1,
+//             duration: 1,
+//             delay: 0.3,
+//             ease: "power3.out"
+//         });
 
-    gsap.fromTo(".f-b", {
-        y: 80,
-        opacity: 0,
-    },
-        {
-            y: 0,
-            opacity: 1,
-            stagger: 0.1,
-            duration: 2,
-            delay: 0.3,
-            ease: "power3.out"
-        });
+//     gsap.fromTo(".f-b", {
+//         y: 80,
+//         opacity: 0,
+//     },
+//         {
+//             y: 0,
+//             opacity: 1,
+//             stagger: 0.1,
+//             duration: 2,
+//             delay: 0.3,
+//             ease: "power3.out"
+//         });
 
-    // hero image animations
-    gsap.fromTo(".img1", {
-        y: 90,
-        opacity: 0,
-    },
-        {
-            y: 0,
-            opacity: 1,
-            duration: 2,
-            delay: 0.3,
-            ease: "power3.out"
-        });
+//     // hero image animations
+//     gsap.fromTo(".img1", {
+//         y: 90,
+//         opacity: 0,
+//     },
+//         {
+//             y: 0,
+//             opacity: 1,
+//             duration: 2,
+//             delay: 0.3,
+//             ease: "power3.out"
+//         });
 
-    // second section  animations
-    gsap.fromTo(".s-text", {
-        y: 60,
-        opacity: 0,
-    },
-        {
-            y: 0,
-            opacity: 1,
-            duration: 2,
-            scrollTrigger: {
-                trigger: "#features",
-                ease: "power3.out",
-                start: "top 80%",
-                toggleActions: "play none none none"
-            }
-        });
-    gsap.from(".feature-box", {
-        y: 70,
-        opacity: 0,
-        scale: 0.5,
-        stagger: 0.3,
-        duration: 0.9,
-        delay: 0.3,
-        scrollTrigger: {
-            trigger: "#features",
-            ease: "power3.out",
-            start: "top 80%",
-            toggleActions: "play none none none"
-        }
+//     // second section  animations
+//     gsap.fromTo(".s-text", {
+//         y: 60,
+//         opacity: 0,
+//     },
+//         {
+//             y: 0,
+//             opacity: 1,
+//             duration: 2,
+//             scrollTrigger: {
+//                 trigger: "#features",
+//                 ease: "power3.out",
+//                 start: "top 80%",
+//                 toggleActions: "play none none none"
+//             }
+//         });
+//     gsap.from(".feature-box", {
+//         y: 70,
+//         opacity: 0,
+//         scale: 0.5,
+//         stagger: 0.3,
+//         duration: 0.9,
+//         delay: 0.3,
+//         scrollTrigger: {
+//             trigger: "#features",
+//             ease: "power3.out",
+//             start: "top 80%",
+//             toggleActions: "play none none none"
+//         }
+//     });
+
+//     // third section  animations
+//     gsap.fromTo(".t-text", {
+//         y: 80,
+//         opacity: 0,
+//     },
+//         {
+//             y: 0,
+//             opacity: 1,
+//             duration: 2,
+//             scrollTrigger: {
+//                 trigger: "#pricing",
+//                 ease: "power3.out",
+//                 start: "top 80%",
+//                 toggleActions: "play none none none"
+//             }
+//         });
+
+//     gsap.fromTo(".pricing-card", {
+//         y: 90,
+//         opacity: 0,
+//         scale: 0.5
+//     },
+//         {
+//             y: 0,
+//             opacity: 1,
+//             scale: 1,
+//             stagger: 0.3,
+//             duration: 2,
+//             delay: 0.4,
+//             scrollTrigger: {
+//                 trigger: "#pricing",
+//                 ease: "power3.out",
+//                 start: "top 80%",
+//                 toggleActions: "play none none none"
+//             }
+//         });
+
+//     // fourth section  animations
+//     gsap.fromTo(".fo-text", {
+//         y: 80,
+//         opacity: 0,
+//     },
+//         {
+//             y: 0,
+//             opacity: 1,
+//             duration: 2,
+//             scrollTrigger: {
+//                 trigger: "#contact",
+//                 ease: "power3.out",
+//                 start: "top 80%",
+//                 toggleActions: "play none none none"
+//             }
+//         });
+
+//     gsap.fromTo(".s-p", {
+//         y: 80,
+//         opacity: 0,
+//     },
+//         {
+//             y: 0,
+//             opacity: 1,
+//             duration: 2,
+//             delay: 0.5,
+//             scrollTrigger: {
+//                 trigger: "#contact",
+//                 ease: "power3.out",
+//                 start: "top 80%",
+//                 toggleActions: "play none none none"
+//             }
+//         })
+
+//     gsap.fromTo(".contact-f", {
+//         y: 80,
+//         opacity: 0,
+//         scale: 0.9
+//     },
+//         {
+//             y: 0,
+//             opacity: 1,
+//             stagger: 0.2,
+//             scale: 1,
+//             duration: 2,
+//             scrollTrigger: {
+//                 trigger: "#contact",
+//                 ease: "power3.out",
+//                 start: "top 80%",
+//                 toggleActions: "play none none none"
+//             }
+//         });
+//     gsap.fromTo(".contact-m", {
+//         y: 80,
+//         opacity: 0,
+//     },
+//         {
+//             y: 0,
+//             opacity: 1,
+//             scale: 0.9,
+//             stagger: 0.2,
+//             duration: 2,
+//             delay: 0.5,
+//             scrollTrigger: {
+//                 trigger: "#contact",
+//                 ease: "power3.out",
+//                 start: "top 80%",
+//                 toggleActions: "play none none none"
+//             }
+//         });
+
+//     // fifth section  animations
+//     gsap.fromTo(".footer-text", {
+//         y: 60,
+//         opacity: 0,
+//     },
+//         {
+//             y: 0,
+//             opacity: 1,
+//             duration: 2,
+//             stagger: 0.3,
+//             scrollTrigger: {
+//                 ease: "power3.out",
+//                 trigger: "#footer",
+//                 start: "top 60%",
+//                 end: "top 40%",
+//                 toggleActions: "play none none none"
+//             }
+//         });
+
+
+
+
+
+// boxes animation
+const box1 = document.querySelector(".box1");
+const box2 = document.querySelector(".box2");
+const box3 = document.querySelector(".box3");
+const box4 = document.querySelector(".box4");
+const box5 = document.querySelector(".box5");
+const box6 = document.querySelector(".box6");
+
+// gsap.fromTo(box1,
+//     {
+//         opacity: 0.1,
+//         scale: 0.1
+//     },
+//     {
+
+//         scale: 1,
+//         opacity: 1,
+//         scrollTrigger: (box1, {
+//             trigger: box1,
+//             start: "top 90%",
+//             end: "top 50%",
+//             ease: "power2.out",
+//             opacity: 1,
+//             scrub: true,
+//             duration: 3,
+//             // markers: true
+//         })
+//     });
+
+
+box1.addEventListener("mouseover", () => {
+    gsap.to([box1], {
+        scale: 1.05,
+        duration: 0.2,
+        y: -2
     });
+})
+box1.addEventListener("mouseout", () => {
+    gsap.to(box1, {
+        scale: 1,
+        duration: 0.2,
+        y: 2
 
-    // // third section  animations
-    // gsap.fromTo(".t-text", {
-    //     y: 80,
-    //     opacity: 0,
-    // },
-    //     {
-    //         y: 0,
-    //         opacity: 1,
-    //         duration: 2,
-    //         scrollTrigger: {
-    //             trigger: "#pricing",
-    //             ease: "power3.out",
-    //             start: "top 80%",
-    //             toggleActions: "play none none none"
-    //         }
-    //     });
+    });
+})
 
-    // gsap.fromTo(".pricing-card", {
-    //     y: 90,
-    //     opacity: 0,
-    //     scale: 0.5
-    // },
-    //     {
-    //         y: 0,
-    //         opacity: 1,
-    //         scale: 1,
-    //         stagger: 0.3,
-    //         duration: 2,
-    //         delay: 0.4,
-    //         scrollTrigger: {
-    //             trigger: "#pricing",
-    //             ease: "power3.out",
-    //             start: "top 80%",
-    //             toggleActions: "play none none none"
-    //         }
-    //     });
+box2.addEventListener("mouseover", () => {
+    gsap.to(box2, {
+        scale: 1.05,
+        duration: 0.2,
+        y: -2
+    });
+})
+box2.addEventListener("mouseout", () => {
+    gsap.to(box2, {
+        scale: 1,
+        duration: 0.2,
+        y: 2
+    });
+})
 
-    // // fourth section  animations
-    // gsap.fromTo(".fo-text", {
-    //     y: 80,
-    //     opacity: 0,
-    // },
-    //     {
-    //         y: 0,
-    //         opacity: 1,
-    //         duration: 2,
-    //         scrollTrigger: {
-    //             trigger: "#contact",
-    //             ease: "power3.out",
-    //             start: "top 80%",
-    //             toggleActions: "play none none none"
-    //         }
-    //     });
+box3.addEventListener("mouseover", () => {
+    gsap.to(box3, {
+        scale: 1.05,
+        duration: 0.2,
+        y: -2
+    });
+})
+box3.addEventListener("mouseout", () => {
+    gsap.to(box3, {
+        scale: 1,
+        duration: 0.2,
+        y: 2
+    });
+})
 
-    // gsap.fromTo(".s-p", {
-    //     y: 80,
-    //     opacity: 0,
-    // },
-    //     {
-    //         y: 0,
-    //         opacity: 1,
-    //         duration: 2,
-    //         delay: 0.5,
-    //         scrollTrigger: {
-    //             trigger: "#contact",
-    //             ease: "power3.out",
-    //             start: "top 80%",
-    //             toggleActions: "play none none none"
-    //         }
-    //     })
+box4.addEventListener("mouseover", () => {
+    gsap.to(box4, {
+        scale: 1.05,
+        duration: 0.2,
+        y: -2
+    });
+})
+box4.addEventListener("mouseout", () => {
+    gsap.to(box4, {
+        scale: 1,
+        duration: 0.2,
+        y: 2
+    });
+})
 
-    // gsap.fromTo(".contact-f", {
-    //     y: 80,
-    //     opacity: 0,
-    //     scale: 0.9
-    // },
-    //     {
-    //         y: 0,
-    //         opacity: 1,
-    //         stagger: 0.2,
-    //         scale: 1,
-    //         duration: 2,
-    //         scrollTrigger: {
-    //             trigger: "#contact",
-    //             ease: "power3.out",
-    //             start: "top 80%",
-    //             toggleActions: "play none none none"
-    //         }
-    //     });
-    // gsap.fromTo(".contact-m", {
-    //     y: 80,
-    //     opacity: 0,
-    // },
-    //     {
-    //         y: 0,
-    //         opacity: 1,
-    //         scale: 0.9,
-    //         stagger: 0.2,
-    //         duration: 2,
-    //         delay: 0.5,
-    //         scrollTrigger: {
-    //             trigger: "#contact",
-    //             ease: "power3.out",
-    //             start: "top 80%",
-    //             toggleActions: "play none none none"
-    //         }
-    //     });
+box5.addEventListener("mouseover", () => {
+    gsap.to(box5, {
+        scale: 1.05,
+        duration: 0.2,
+        y: -2
+    });
+})
+box5.addEventListener("mouseout", () => {
+    gsap.to(box5, {
+        scale: 1,
+        duration: 0.2,
+        y: 2
+    });
+})
 
-    // // fifth section  animations
-    // gsap.fromTo(".footer-text", {
-    //     y: 60,
-    //     opacity: 0,
-    // },
-    //     {
-    //         y: 0,
-    //         opacity: 1,
-    //         duration: 2,
-    //         stagger: 0.3,
-    //         scrollTrigger: {
-    //             ease: "power3.out",
-    //             trigger: "#footer",
-    //             start: "top 60%",
-    //             end: "top 40%",
-    //             toggleActions: "play none none none"
-    //         }
-    //     });
+box6.addEventListener("mouseover", () => {
+    gsap.to(box6, {
+        scale: 1.05,
+        duration: 0.2,
+        y: -2
+    });
+})
+box6.addEventListener("mouseout", () => {
+    gsap.to(box6, {
+        scale: 1,
+        duration: 0.2,
+        y: 2
+    });
+})
 
+// price card animation
+const plan1 = document.querySelector(".plan1");
+const plan2 = document.querySelector(".plan2");
+const plan3 = document.querySelector(".plan3");
 
+plan1.addEventListener("mouseover", () => {
+    gsap.to(plan1, {
+        scale: 1.05,
+        duration: 0.2,
+        y: -2
+    });
+})
+plan1.addEventListener("mouseout", () => {
+    gsap.to(plan1, {
+        scale: 1,
+        duration: 0.2,
+        y: 2
+    });
+})
 
+plan2.addEventListener("mouseover", () => {
+    gsap.to(plan2, {
+        scale: 1.05,
+        duration: 0.2,
+        y: -2
+    });
+})
+plan2.addEventListener("mouseout", () => {
+    gsap.to(plan2, {
+        scale: 1,
+        duration: 0.2,
+        y: 2
+    });
+})
 
+plan3.addEventListener("mouseover", () => {
+    gsap.to(plan3, {
+        scale: 1.05,
+        duration: 0.2,
+        y: -2
+    });
+})
+plan3.addEventListener("mouseout", () => {
+    gsap.to(plan3, {
+        scale: 1,
+        duration: 0.2,
+        y: 2
+    });
+})
 
-    // boxes animation
-    const box1 = document.querySelector(".box1");
-    const box2 = document.querySelector(".box2");
-    const box3 = document.querySelector(".box3");
-    const box4 = document.querySelector(".box4");
-    const box5 = document.querySelector(".box5");
-    const box6 = document.querySelector(".box6");
+// current year
+const year = document.querySelector("#year");
 
-    // gsap.fromTo(box1,
-    //     {
-    //         opacity: 0.1,
-    //         scale: 0.1
-    //     },
-    //     {
+const date = new Date();
+const years = date.getFullYear();
 
-    //         scale: 1,
-    //         opacity: 1,
-    //         scrollTrigger: (box1, {
-    //             trigger: box1,
-    //             start: "top 90%",
-    //             end: "top 50%",
-    //             ease: "power2.out",
-    //             opacity: 1,
-    //             scrub: true,
-    //             duration: 3,
-    //             // markers: true
-    //         })
-    //     });
-
-
-    box1.addEventListener("mouseover", () => {
-        gsap.to([box1], {
-            scale: 1.05,
-            duration: 0.2,
-            y: -2
-        });
-    })
-    box1.addEventListener("mouseout", () => {
-        gsap.to(box1, {
-            scale: 1,
-            duration: 0.2,
-            y: 2
-
-        });
-    })
-
-    box2.addEventListener("mouseover", () => {
-        gsap.to(box2, {
-            scale: 1.05,
-            duration: 0.2,
-            y: -2
-        });
-    })
-    box2.addEventListener("mouseout", () => {
-        gsap.to(box2, {
-            scale: 1,
-            duration: 0.2,
-            y: 2
-        });
-    })
-
-    box3.addEventListener("mouseover", () => {
-        gsap.to(box3, {
-            scale: 1.05,
-            duration: 0.2,
-            y: -2
-        });
-    })
-    box3.addEventListener("mouseout", () => {
-        gsap.to(box3, {
-            scale: 1,
-            duration: 0.2,
-            y: 2
-        });
-    })
-
-    box4.addEventListener("mouseover", () => {
-        gsap.to(box4, {
-            scale: 1.05,
-            duration: 0.2,
-            y: -2
-        });
-    })
-    box4.addEventListener("mouseout", () => {
-        gsap.to(box4, {
-            scale: 1,
-            duration: 0.2,
-            y: 2
-        });
-    })
-
-    box5.addEventListener("mouseover", () => {
-        gsap.to(box5, {
-            scale: 1.05,
-            duration: 0.2,
-            y: -2
-        });
-    })
-    box5.addEventListener("mouseout", () => {
-        gsap.to(box5, {
-            scale: 1,
-            duration: 0.2,
-            y: 2
-        });
-    })
-
-    box6.addEventListener("mouseover", () => {
-        gsap.to(box6, {
-            scale: 1.05,
-            duration: 0.2,
-            y: -2
-        });
-    })
-    box6.addEventListener("mouseout", () => {
-        gsap.to(box6, {
-            scale: 1,
-            duration: 0.2,
-            y: 2
-        });
-    })
-
-    // price card animation
-    const plan1 = document.querySelector(".plan1");
-    const plan2 = document.querySelector(".plan2");
-    const plan3 = document.querySelector(".plan3");
-
-    plan1.addEventListener("mouseover", () => {
-        gsap.to(plan1, {
-            scale: 1.05,
-            duration: 0.2,
-            y: -2
-        });
-    })
-    plan1.addEventListener("mouseout", () => {
-        gsap.to(plan1, {
-            scale: 1,
-            duration: 0.2,
-            y: 2
-        });
-    })
-
-    plan2.addEventListener("mouseover", () => {
-        gsap.to(plan2, {
-            scale: 1.05,
-            duration: 0.2,
-            y: -2
-        });
-    })
-    plan2.addEventListener("mouseout", () => {
-        gsap.to(plan2, {
-            scale: 1,
-            duration: 0.2,
-            y: 2
-        });
-    })
-
-    plan3.addEventListener("mouseover", () => {
-        gsap.to(plan3, {
-            scale: 1.05,
-            duration: 0.2,
-            y: -2
-        });
-    })
-    plan3.addEventListener("mouseout", () => {
-        gsap.to(plan3, {
-            scale: 1,
-            duration: 0.2,
-            y: 2
-        });
-    })
-
-    // current year
-    const year = document.querySelector("#year");
-
-    const date = new Date();
-    const years = date.getFullYear();
-
-    year.textContent = years;
+year.textContent = years;
 
 
 
-    ScrollTrigger.refresh();
-});
+//     ScrollTrigger.refresh();
+// });
